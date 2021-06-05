2818c0cbd6fe9d8b2c7a3c1b680789d9ed2673e7
nuri1234 <nuransh@ac.sce.ac.il>
2021-06-05 15:24:43 +0300

    git commit -m "BSPM2021T16" unitest

--- functions.py
+++ functions.py
@@ -10,12 +10,12 @@ users = mysql.connector.connect(
 
 
 
-def insert_to_table(p,n,u):
+def insert_to_table(p,n,us):
     connect = users
     cc = connect.cursor(buffered=True)
     sql = '''INSERT INTO projects VALUES(%s,%s,%s)
      '''
-    dats_tuple = (p, n, u)
+    dats_tuple = (p, n, us)
     try:
         cc.execute(sql, dats_tuple)
         return 1
